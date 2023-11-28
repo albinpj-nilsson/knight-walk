cb8b161b07cc816b4ae734cdc9a66b8972ace211
albinnilsson <albinnilsson2001@gmail.com>
2023-11-28 12:22:33 +0100

    Uppdaterat kodskelett mha engine.

--- engine.py
+++ engine.py
@@ -1,16 +1,22 @@
-"""I create the engine for the game and document using Google style docstrings,
+"""I create the engine for the game reliant upon tkinter. Google style docstrings,
 see e.g. https://github.com/google/styleguide/blob/gh-pages/pyguide.md#38-comments-and-docstrings
 """
 
+__author__ = "Albin Nilsson"
+__copyright__ = "Copyright 2023, Kth"
+__license__ = "CC0"
 
+from tkinter import *
+
+# Classes for the game
 class Square:
     """Defines logic for a checkboard square
 
-    Attributes:
-        """
+    Attributes: ...
+    """
 
     def __init__(self):
-        """Initializes the instance based on .
+        """Initializes the instance based on ...
 
         Args:
             str1 (str): The string to be reversed
@@ -18,8 +24,6 @@ class Square:
         Returns:
             reverse(str1): The string which gets reversed
         """
-        self.
-
         pass
 
     def click_event(self):
@@ -31,7 +35,50 @@ class Square:
         Returns:
             reverse(str1): The string which gets reversed
         """
+        pass
 
 
 class Chessboard:
-    """Builds functions for the chessboard and game logic"""
+    """Builds functions for the chessboard and game logic
+
+    Attributes: ...
+    """
+    def __init__(self):
+        """Initializes the instance based on ...
+
+        Args:
+            str1 (str): The string to be reversed
+
+        Returns:
+            reverse(str1): The string which gets reversed
+        """
+        pass
+    def create_board(self):
+        """Iterates across the window and to create instances for the Square class
+
+        Args:
+            str1 (str): The string to be reversed
+
+        Returns:
+            reverse(str1): The string which gets reversed
+        """
+        pass
+
+    def valid_moves(self, position):
+        """Calculates valid moves based on current position.
+
+        Args:
+            str1 (str): The string to be reversed
+
+        Returns:
+            valid_moves: List of valid coordinates
+        """
+    def compute_longest_route():
+        """Computes the longest route for knight.
+
+        Args:
+            str1 (str): The string to be reversed
+
+        Returns:
+            longest_route: List of positions in order of the path
+        """
