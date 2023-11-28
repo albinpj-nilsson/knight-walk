7c587b85c1bddca6ba27baec927d5ff7d3791fae
albinnilsson <albinnilsson2001@gmail.com>
2023-11-28 11:28:28 +0100

    Uppdaterade spec, skapade engine, utförlig dokumentation.

---
+++ engine.py
@@ -0,0 +1,37 @@
+"""I create the engine for the game and document using Google style docstrings,
+see e.g. https://github.com/google/styleguide/blob/gh-pages/pyguide.md#38-comments-and-docstrings
+"""
+
+
+class Square:
+    """Defines logic for a checkboard square
+
+    Attributes:
+        """
+
+    def __init__(self):
+        """Initializes the instance based on .
+
+        Args:
+            str1 (str): The string to be reversed
+
+        Returns:
+            reverse(str1): The string which gets reversed
+        """
+        self.
+
+        pass
+
+    def click_event(self):
+        """Updates a square when pressed
+
+        Parameters:
+            str1 (str): The string to be reversed
+
+        Returns:
+            reverse(str1): The string which gets reversed
+        """
+
+
+class Chessboard:
+    """Builds functions for the chessboard and game logic"""
