c0bfc2dd636334fcd72ca9be76ddd6645724ca91
albinnilsson <albinnilsson2001@gmail.com>
2023-12-01 15:04:59 +0100

    Nu fungerar spelet. Uppdaterade även print funktionen.

--- engine.py
+++ engine.py
@@ -32,32 +32,47 @@ class Chessboard:
     def print_board(self):
         """Prints chessboard to terminal by iterating through board
         """
-        print("------------------------------------------")
-        print("|    ---------------------------------   |")
+        print("--------------------------------------------------")
+        print("|    -----------------------------------------   |")
         for r in range(9, 1, -1): # Iterate backwards to get chessboard row numbers right
             print(f"|{r-1}   |",end="") # Adds row numbers for each row
             for c in range(2, 10):
-                print(f" {' ' if self.board[r][c] == 0 else self.board[r][c]} |", end="") # Prints squares where
+                square_value = self.board[r][c]
+                if square_value == 0:
+                    value_str = " "
+                else:
+                    value_str = str(square_value)
+                spaces_to_add = 4 - len(value_str)
+                print(f"{' ' * (spaces_to_add // 2)}{value_str}{' ' * (spaces_to_add - spaces_to_add // 2)}|", end="") # Prints squares where
                 # knight has not been as spaces and all other squares as the number of the matrix position
                 if c == 9: # Add rightmost board edge when loop reaches last column
                     print("   |")
-            print("|    ---------------------------------   |") # Adds lines between rows
-        print("|      A   B   C   D   E   F   G   H     |")
-        print("------------------------------------------")
+            print("|    -----------------------------------------   |") # Adds lines between rows
+        print("|       A    B    C    D    E    F    G    H     |")
+        print("--------------------------------------------------")
 
 
-    def valid_moves(self, row, column):
+    def valid_moves(self, current_row, current_col):
         """Calculates valid moves based on current position and board characteristics.
 
         Args:
             row (int): Input row
-            column (int): Input column
+            col (int): Input column
 
         Returns:
-            moves (list): List of valid moves computed from input position
+            valid_moves (list): List of valid moves computed from input position
         """
-        step = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
-        pass
+        valid_moves_list = []
+        steps = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
+        for r, c in steps:
+            new_row, new_col = current_row + r, current_col + c
+            if 2 <= new_row <= 9 and 2 <= new_col <= 9 and self.board[new_row][new_col] == 0: # Knight only allowed
+                # to go to squares that is within the board, and previously unvisited
+                valid_moves_list.append((new_row, new_col))
+        return valid_moves_list
+
+
+
     def move_knight(self, start_row, start_column):
         """Moves the knight by checking that the move is valid, and ticks up a move counter.
         Alters board by adding this move counter to corresponding position.
@@ -69,7 +84,25 @@ class Chessboard:
         Returns:
             None
         """
-        pass
+        move_number = 1
+        self.board[start_row][start_column] = move_number # We represent the squares the knight has visited with its number
+
+        while True:
+            moves = self.valid_moves(start_row, start_column)
+
+            if not moves:
+                break # Exit loop when there are no more valid squares to visit
+
+            chosen_move = random.choice(moves) # Randomly chooses a move
+            move_number += 1
+            self.board[chosen_move[0]][chosen_move[1]] = move_number
+
+            start_row, start_column = chosen_move # The chosen move becomes input for the next loop
+
+
+
+
+
 
 
 def save_high_score(steps):
@@ -84,5 +117,18 @@ def save_high_score(steps):
     pass
 
 if __name__ == "__main__":
-    knight = Chessboard()
-    knight.print_board()
+    chessboard = Chessboard()
+    chessboard.print_board()
+
+    while True: # User input must be valid
+        start_position = input("Type your starting square (e.g., E4): ")
+        start_column = ord(start_position[0].upper()) - ord('A') + 2
+        start_row = int(start_position[1]) + 1
+
+        if 2 <= start_row <= 9 and 2 <= start_column <= 9:
+            break
+        else:
+            print("Invalid starting position. Please try again.")
+
+    chessboard.move_knight(start_row, start_column)
+    chessboard.print_board()
