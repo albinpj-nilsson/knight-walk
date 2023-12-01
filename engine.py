be275f040a01f37684de74cf0cb6cc64267a7a22
albinnilsson <albinnilsson2001@gmail.com>
2023-12-01 11:58:02 +0100

    Skapat board, skapat fungerande metod print_board

--- engine.py
+++ engine.py
@@ -18,15 +18,33 @@ class Chessboard:
         """Initializes the instance by creating a chessboard as a nested list of integers
 
         Args:
-            board (list): 12x12 matrix filled with empty spaces for "real 8x8 board", while squares outside of this are set to -1
+            board (list): 12x12 matrix filled with empty spaces (zeroes) for "real 8x8 board", while squares outside of this are set to -1
 
         Returns:
             None
         """
+        self.board = [[-1] * 12 for _ in range(12)] # Create a 12x12 matrix and initialize with -1 for the entire board
+
+        for r in range(2, 10): # Set the inner part of the board to 0
+            for c in range(2, 10):
+                self.board[r][c] = 0
+
     def print_board(self):
         """Prints chessboard to terminal by iterating through board
         """
-        pass
+        print("------------------------------------------")
+        print("|    ---------------------------------   |")
+        for r in range(9, 1, -1): # Iterate backwards to get chessboard row numbers right
+            print(f"|{r-1}   |",end="") # Adds row numbers for each row
+            for c in range(2, 10):
+                print(f" {' ' if self.board[r][c] == 0 else self.board[r][c]} |", end="") # Prints squares where
+                # knight has not been as spaces and all other squares as the number of the matrix position
+                if c == 9: # Add rightmost board edge when loop reaches last column
+                    print("   |")
+            print("|    ---------------------------------   |") # Adds lines between rows
+        print("|      A   B   C   D   E   F   G   H     |")
+        print("------------------------------------------")
+
 
     def valid_moves(self, row, column):
         """Calculates valid moves based on current position and board characteristics.
@@ -63,3 +81,8 @@ def save_high_score(steps):
     Returns:
         None
     """
+    pass
+
+if __name__ == "__main__":
+    knight = Chessboard()
+    knight.print_board()
